fbe950bbd23fb80f178a3fbd9ea62709c51bed28
Piyush <jangidpiyush16011@gmail.com>
2024-09-29 13:20:33 +0530

    Add files via upload

---
+++ #6 (Clothing Brand)/script.js
@@ -0,0 +1,6 @@
+function changeLanguage() {
+    const language = document.getElementById('language').value;
+    alert(`Language changed to: ${language}`);
+    // Here you can add logic to load content based on the selected language
+  }
+  
